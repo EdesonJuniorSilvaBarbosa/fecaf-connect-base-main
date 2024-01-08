1b3239c2c77a06068f83f30e7954e2b5f7608298
Edeson Junior <edeson.jsbarbosa@hotmail.com>
2024-01-08 16:05:34 -0300

    Enviando Dados com os Métodos POST e PUT

--- js/contato.js
+++ js/contato.js
@@ -1,38 +1,66 @@
 'user strict'
 import { preview } from "./preview.js"
-import { getContato } from "./contatos.js"
+import { getContato, postContato, putContato } from "./contatos.js"
 
-async function buscarContato(){
-    const id = localStorage.getItem('id')
+async function buscarContato(id){
     const contato = await getContato(id)
     return contato 
 }
 
-const habilitarCampo = campo => campo.disabled = false
+const habilitarCampo = campo => campo.disabled = !campo.disabled
+
 const trocarBotao = botao => botao.classList.toggle('oculto')
+
 function habilitarFormulario(){
     document.querySelectorAll('input').forEach(habilitarCampo)
     document.querySelectorAll('.button').forEach(trocarBotao)
 }
 
 async function preencherFormulario(){
-    const contato = await buscarContato()
-    if (contato == undefined){
+
+    const id = localStorage.getItem('id')
+
+    if (id == undefined){
+        document.getElementById('salvar').dataset.tipo = 'novo'
         habilitarFormulario()
         return false
+    }else{
+        const contato = await buscarContato(id)
+        document.getElementById('nome').value = contato.nome
+        document.getElementById('email').value = contato.email
+        document.getElementById('celular').value = contato.celular
+        document.getElementById('endereco').value = contato.endereco
+        document.getElementById('cidade').value = contato.cidade
+        document.getElementById('imagePreview').src = contato.foto
     }
-    document.getElementById('nome').value = contato.nome
-    document.getElementById('email').value = contato.email
-    document.getElementById('celular').value = contato.celular
-    document.getElementById('endereco').value = contato.endereco
-    document.getElementById('cidade').value = contato.cidade
-    document.getElementById('imagePreview').src = contato.foto
+}
+
+function salvarContato({target}){
+    const contato = {
+        "nome": document.getElementById('nome').value,
+        "email": document.getElementById('email').value,
+        "celular": document.getElementById('celular').value,
+        "endereco": document.getElementById('endereco').value,
+        "cidade": document.getElementById('cidade').value,
+        "foto": document.getElementById('imagePreview').src
+    }
+
+    if(target.dataset.tipo == 'novo'){
+        postContato(contato)
+        alert('Cadastro com sucesso !!!')
+        window.location.href = '/'
+    }else{
+        contato.id = localStorage.getItem('id')
+        putContato(contato)
+        habilitarFormulario()
+    }
 }
 
 preencherFormulario()
 
 document.getElementById('editar').addEventListener('click', habilitarFormulario)
 document.getElementById('inputFile').addEventListener('change', () => preview('inputFile', 'imagePreview'))
+document.getElementById('salvar').addEventListener('click', salvarContato)
 
 
 
--- js/contatos.js
+++ js/contatos.js
@@ -16,9 +16,33 @@ export async function getContato(id){
     return data
 }
 
+export async function postContato(contato){
+    const url = 'https://backend-fecaf-connect.vercel.app/contatos'
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contato)
+    }
 
+    const response = await fetch(url, options)
+    return response.ok
+}
 
+export async function putContato(contato){
+    const url = `https://backend-fecaf-connect.vercel.app/contatos/${contato.id}`
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contato)
+    }
 
+    const response = await fetch(url, options)
+    return response.ok
+}
 
 
 
